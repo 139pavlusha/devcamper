a9e478be1dcd78dd35b36a3c4d663ffc8e2fc73a
139pavlusha <pavel1grek@gmail.com>
2020-08-04 18:17:39 +0300

    Mongoose connect to DB and catch Error

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-const logger = require('./middleware/logger');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to DB
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -44,4 +46,13 @@ app.delete('/api/v1/bootcamps/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`));
+const server = app.listen(
+    PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
+);
+
+// Handle unhandeled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
