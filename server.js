ac8a6a5c7a45739c891ab05f1759d814a9a1e24a
139pavlusha <pavel1grek@gmail.com>
2020-08-09 23:20:28 +0300

    create routers and GET for courses

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
+
 
 const app = express();
 
@@ -27,29 +29,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
 app.use(errorHandler);
-
-
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Show  bootcamp ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Create new bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
 
 const PORT = process.env.PORT || 5000;
 
