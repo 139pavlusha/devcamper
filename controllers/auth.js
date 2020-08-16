f7fd295d79a731c79ed7467ffb6aa358f4b4090d
139pavlusha <pavel1grek@gmail.com>
2020-08-16 16:54:51 +0300

    reset password send email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/User');
 const asyncHandler = require('../middleware/async');
-const processMultipart = require('express-fileupload/lib/processMultipart');
+const sendEmail = require('../utils/sendEmail');
 
 
 // @desc    Register user
@@ -75,7 +75,28 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
     await user.save({ validateBeforeSave: false });
 
-    res.status(200).json({ success: true, data: user });
+    // Create reset url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+    const message = `You are receiving this email because you (or someone else) has requested the reset a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password reset token',
+            message
+        });
+
+        res.status(200).json({ success: true, data: 'Email sent' });
+    } catch (err) {
+        console.log(err);
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorResponse('Email could not be sent', 500));
+    }
 });
 
 
