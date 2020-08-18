338763198ff97a94b1bfef58f91042c180f30d65
139pavlusha <pavel1grek@gmail.com>
2020-08-18 19:27:23 +0300

    download security packages and CORS

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const colors = require('colors');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
@@ -55,13 +58,25 @@ app.use(helmet());
 // Prevent XSS scrypting attacks
 app.use(xss());
 
+// rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/users', users);
 app.use('/api/v1/reviews', reviews);
-
 
 
 app.use(errorHandler);
