46c2f062d8e8dea6d2b18bfaebad123f510ca205
139pavlusha <pavel1grek@gmail.com>
2020-08-18 18:40:46 +0300

    clear cookies for logout (just for show)

--- routes/auth.js
+++ routes/auth.js
@@ -7,12 +7,14 @@ const {
     forgotPassword,
     resetPassword,
     updateDetalis,
-    updatePassword } = require('../controllers/auth');
+    updatePassword,
+    logout } = require('../controllers/auth');
 
 const router = express.Router();
 
 router.post('/register', register)
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
